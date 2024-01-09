6c329399bfe530140e987c601faa845f907f513d
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2024-01-09 13:03:23 -0800

    Fix inital atom values used on first render

--- src/withJotai.tsx
+++ src/withJotai.tsx
@@ -1,5 +1,5 @@
 import { Provider as JotaiProvider, useAtomValue, useSetAtom } from 'jotai';
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useMemo, useState } from 'react';
 
 import { addons, makeDecorator } from '@storybook/addons';
 
@@ -14,7 +14,7 @@ interface CommonProps {
 }
 
 const StorybookAddonJotaiInContext = ({ parameters, children }: CommonProps) => {
-  const newSetters: typeof setters = {};
+  const setters: Record<string, Function> = {};
   const parametersValue: Parameters<any> = useMemo(
     () => (typeof parameters === 'function' ? parameters() : parameters || {}),
     [],
@@ -24,7 +24,7 @@ const StorybookAddonJotaiInContext = ({ parameters, children }: CommonProps) => 
   const initialValues = typeof valuesFuncOrValue === 'function' ? valuesFuncOrValue() : valuesFuncOrValue;
   const channel = addons.getChannel();
   const currentAtomValues: Record<string, any> = {};
-  const [setters, setSetters] = useState<Record<string, Function> | undefined>();
+  const [initalized, setInitialized] = useState(false);
 
   const userNote = (note: string) => {
     channel.emit(EVENTS.SET_INITIAL_VALUES, { [NOTE]: note });
@@ -35,23 +35,23 @@ const StorybookAddonJotaiInContext = ({ parameters, children }: CommonProps) => 
   if (!atoms) return userNote('parameters.jotai.atoms not defined');
   if (!initialValues) return userNote('parameters.jotai.values not defined');
 
-  // Collect all the atom setters, must be done outside of any hooks of course.
+  // Collect all the atom setters, must be done outside of any hooks and ifs.
+  // This value will not change between renders because we momoized the parameters.
   Object.keys(atoms).forEach((key) => {
-    newSetters[key] = useSetAtom(atoms[key]);
+    setters[key] = useSetAtom(atoms[key]);
   });
 
-  // Preserve setters.
-  if (!setters) {
-    setSetters(newSetters);
-  }
+  // Set initial values to our atom setters, doing this outside of any hooks
+  // helps us avoid original atom values from being picked up by chilren.
+  if (!initalized) {
+    setInitialized(true);
 
-  // Will be triggered when setters are known, this is where we assign initial
-  // values to atoms.
-  useEffect(() => {
+    // Will be triggered when setters are known, this is where we assign initial
+    // values to atoms.
     Object.entries(setters).forEach(([key, setter]) => {
       setter(initialValues[key]);
     });
-  }, [setters, initialValues]);
+  }
 
   // We are looping through the atoms to get their values, so the expectation
   // here is that the number of atoms doesn't change between renders,
